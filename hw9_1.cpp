4c974bc3dc5c719c2f06c863008d8eb57885b3aa
rainstorm31 <zoewang1031@yahoo.com>
2019-10-30 22:00:07 -0700

    try to use git for main

--- hw9_1.cpp
+++ hw9_1.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!try git !!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
